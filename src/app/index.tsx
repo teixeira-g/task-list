f28be2fa7a9ca911abf01e88ad95e7b55807ece0
gabriel-teixeira <gabriel.txr00@gmail.com>
2024-09-25 23:19:14 -0300

    Add Button

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,13 +1,13 @@
 import { StyleSheet, View } from "react-native";
-import { AddButton, CloseButton, ConfirmButton } from "@/components/Buttons";
+import { AddButton, } from "@/components/Buttons";
 import { colors } from "@/styles/colors";
 import { Header } from "@/components/Header";
-import { InputTasks } from "@/components/Inputs";
 
 export default function LoginScreen() {
   return (
     <View style={styles.container}>
       <Header />
+      <AddButton/>
     </View>
   );
 }
