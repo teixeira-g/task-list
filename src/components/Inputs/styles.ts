1de32f0d3fb3ee24450a035298816a6591416a42
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-09-25 22:40:46 -0300

    add Buttons

--- src/components/Inputs/styles.ts
+++ src/components/Inputs/styles.ts
@@ -1,17 +1,18 @@
-import styled from 'styled-components/native';
+import { colors } from "@/styles/colors";
+import styled from "styled-components/native";
 
 export const SInput = styled.TextInput`
-    flex-direction: row;
-    width: 362px;
-    height: 68px;
-    border-radius: 20px;
-    background-color: #B8C0C2;
+  flex-direction: row;
+  width: 362px;
+  height: 68px;
+  border-radius: 20px;
+  background-color: ${colors.gray[300]};
 `;
 
 export const BInput = styled.TextInput`
-    flex-direction: row;
-    width: 362px;
-    height: 292px;
-    border-radius: 20px;
-    background-color: #B8C0C2;
+  flex-direction: row;
+  width: 362px;
+  height: 292px;
+  border-radius: 20px;
+  background-color: ${colors.gray[300]};
 `;
