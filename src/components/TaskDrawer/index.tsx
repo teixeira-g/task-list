deda8c444921e977ce086373d959cf523ee63b0a
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-10-03 17:29:15 -0300

    update TaskDrawer

--- src/components/TaskDrawer/index.tsx
+++ src/components/TaskDrawer/index.tsx
@@ -1,51 +1,56 @@
-import {FlatList, TouchableOpacity, View, Text} from "react-native";
+import React, { useState } from "react";
+import { FlatList, TouchableOpacity, Text } from "react-native";
 import { Feather } from "@expo/vector-icons";
 import { colors } from "@/styles/colors";
-import { TaskContainer } from "./styles";
-import { H2DarkText } from "@/styles/global";
-import React, {useState} from "react";
-import {TaskCard} from "@/components/TaskCard";
+import { TaskContainer, HeaderContainer } from "./styles";
+import { DescriptionText, H2DarkText } from "@/styles/global";
+import { TaskCard } from "@/components/TaskCard";
 
 type Props = {
-    title: string;
-    onPress: () => void;
-    isOpen: boolean;
+  title: string;
+  onPress: () => void;
+  isOpen: boolean;
 };
 
-const [tasks, setTasks] = useState<{title: string; description: string; check: boolean;}[]>([]);
-const [taskText, setTaskText] = useState("");
-
 export const TaskDrawer = ({ title, onPress, isOpen }: Props) => {
-    return (
-        <TaskContainer>
-            <H2DarkText>{title}</H2DarkText>
+  const [tasks, setTasks] = useState<
+    { title: string; description: string; check: boolean }[]
+  >([]);
+  const [taskText, setTaskText] = useState("");
 
-            <FlatList
-                data={tasks}
-                keyExtractor={(item, index) => index.toString()}
-                renderItem={
-                    (item) => (
-                        <TaskCard title={""} description={""} isCompleted={false}/>
-                    )
-                }
-                ListEmptyComponent={() => (
-                    <View>
-                        <Text>Você ainda não adicionou nenhuma tarefa</Text>
-                    </View>
-                )}
-            />
+  return (
+    <TaskContainer>
+      <HeaderContainer>
+        <H2DarkText>{title}</H2DarkText>
+        <TouchableOpacity
+          onPress={onPress}
+          activeOpacity={0.7}
+          hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
+        >
+          <Feather
+            name={isOpen ? "chevron-up" : "chevron-down"}
+            size={34}
+            color={isOpen ? colors.gray[600] : colors.gray[400]}
+          />
+        </TouchableOpacity>
+      </HeaderContainer>
 
-            <TouchableOpacity
-                onPress={onPress}
-                activeOpacity={0.7}
-                hitSlop={{ top: 20, right: 20, bottom: 20, left: 20 }}
-            >
-                <Feather
-                    name={isOpen ? "chevron-up" : "chevron-down"}
-                    size={34}
-                    color={isOpen ? colors.gray[600] : colors.gray[400]}
-                />
-            </TouchableOpacity>
-        </TaskContainer>
-    );
+      <FlatList
+        data={tasks}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({ item }) => (
+          <TaskCard
+            title={item.title}
+            description={item.description}
+            isCompleted={item.check}
+          />
+        )}
+        ListEmptyComponent={() => (
+          <DescriptionText>
+            <Text>Você ainda não adicionou nenhuma tarefa</Text>
+          </DescriptionText>
+        )}
+      />
+    </TaskContainer>
+  );
 };
