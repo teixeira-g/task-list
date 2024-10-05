fe577eee39a34fe0703cbf75fe646625a5fcfd50
Bruno Galvão <bgalvaomiranda302@gmail.com>
2024-10-04 23:02:44 -0300

    feat(LoginScreen): add input validation to username field

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React from "react";
 import {
   View,
   StyleSheet,
@@ -6,21 +6,35 @@ import {
   Keyboard,
 } from "react-native";
 import { useRouter } from "expo-router";
+
 import { colors } from "@/styles/colors";
+import { H2DarkText, NotificationText } from "@/styles/global";
 import { SmallInput } from "@/components/Inputs";
-import { H2DarkText } from "@/styles/global";
 import { AuthButton } from "@/components/Buttons";
 
+import * as Yup from "yup";
+import { Formik } from "formik";
+
+type LoginFormValues = {
+  username: string;
+};
+
 export default function Login() {
-  const [username, setUsername] = useState("");
   const router = useRouter();
 
-  const handleLogin = () => {
+  const handleLogin = (values: LoginFormValues) => {
+    const { username } = values;
     router.replace({
       pathname: "/home",
       params: { username },
     });
   };
+
+  const validationSchema = Yup.object().shape({
+    username: Yup.string()
+      .required("*Necessário fornecer um nome")
+      .max(26, "*Limite de 26 caracteres"),
+  });
 
   return (
     <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
@@ -28,14 +42,40 @@ export default function Login() {
         <H2DarkText style={{ fontSize: 30, color: colors.gray[600] }}>
           Digite seu nome
         </H2DarkText>
-        <SmallInput
-          value={username}
-          onChangeText={setUsername}
-          placeholder={"Nome de usuário"}
-        />
-        <View style={{ marginTop: 20 }}>
-          <AuthButton title="Entrar" onPress={handleLogin} />
-        </View>
+
+        <Formik
+          initialValues={{ username: "" }}
+          validationSchema={validationSchema}
+          onSubmit={handleLogin}
+        >
+          {({
+            handleChange,
+            handleBlur,
+            handleSubmit,
+            values,
+            errors,
+            touched,
+          }) => (
+            <>
+              <SmallInput
+                value={values.username}
+                onChangeText={handleChange("username")}
+                onBlur={handleBlur("username")}
+                placeholder={"Nome de usuário"}
+              />
+              {errors.username && touched.username && (
+                <NotificationText
+                  style={{ fontSize: 14, color: colors.red, marginTop: 20 }}
+                >
+                  {errors.username}
+                </NotificationText>
+              )}
+              <View style={{ marginTop: 20 }}>
+                <AuthButton title="Entrar" onPress={handleSubmit} />
+              </View>
+            </>
+          )}
+        </Formik>
       </View>
     </TouchableWithoutFeedback>
   );
